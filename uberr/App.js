f0936edd417a816206a2d69e617260b2ce9ce1d4
Shivansh <shivansh01308@gmail.com>
2024-01-13 21:49:03 +0530

    Initial setup completed. Redux store created. Navigation slice created.

---
+++ uberr/App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { store } from './store';
+
+export default function App() {
+  return (
+    <Provider store={store}>
+    <View style={styles.container}>
+      <Text>Uber banega panchoo ehhlloooooo jdbsdsdsdsiwh</Text>
+      <StatusBar style="auto" />
+    </View>
+    </Provider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
