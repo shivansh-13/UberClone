8641be16637d45bdc37a2b7594881aebb5476558
Shivansh <shivansh01308@gmail.com>
2024-01-14 21:51:43 +0530

    added where to lat long coord in map render

--- uberr/screens/HomeScreen.js
+++ uberr/screens/HomeScreen.js
@@ -2,13 +2,27 @@ import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native';
 import React from 'react';
 import tw from 'tailwind-react-native-classnames';
 import NavOptions from '../components/NavOptions';
-import { GooglePlacesAutocomplete  } from 'react-native-google-places-autocomplete';
-import {GOOGLE_MAPS_APIKEY} from "@env"
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { GOOGLE_MAPS_APIKEY } from "@env";
 import { useDispatch } from 'react-redux';
 import { setDestination, setOrigin } from '../slices/navSlice';
 
 const HomeScreen = () => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
+
+  const getPlaceDetails = async (placeId) => {
+    try {
+      const response = await fetch(
+        `https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}&key=${GOOGLE_MAPS_APIKEY}`
+      );
+      const data = await response.json();
+      return data.result.geometry.location;
+    } catch (error) {
+      console.error("Error fetching place details:", error);
+      return null;
+    }
+  };
+
   return (
     <SafeAreaView style={[tw`bg-white h-full`]}>
       <View style={[tw`p-5`]}>
@@ -16,36 +30,42 @@ const HomeScreen = () => {
           source={{ uri: 'https://links.papareact.com/gzs' }}
           style={{ width: 100, height: 100, resizeMode: 'contain' }}
         />
-        <GooglePlacesAutocomplete 
-        placeholder='Where From?'
-        styles={{
-            container:{
-                flex:0,
+        <GooglePlacesAutocomplete
+          placeholder='Where From?'
+          styles={{
+            container: {
+              flex: 0,
             },
-            textInput:{
-                fontSize:18,
+            textInput: {
+              fontSize: 18,
             }
-        }}
-        onPress={(data, details = null) => {
-            console.log(data, details);
-            dispatch(setOrigin({
-                location:details.geometry.location,
-                description:data.description,
-            }))
-            dispatch(setDestination(null))
-        }}
-        query={
-             {key:GOOGLE_MAPS_APIKEY,
-             language:"en"}
-        }
-        minLength={2}
-        nearbyPlacesAPI='GooglePlacesSearch'
-        returnKeyType={'search'}
-        debounce={400}
-        enablePoweredByContainer={false}
+          }}
+          onPress={async (data, details = null) => {
+            console.log(data);
+            console.log(details);
+            const location = await getPlaceDetails(details.place_id);
+            if (location) {
+              dispatch(setOrigin({
+                location,
+                description: data.description,
+              }));
+              dispatch(setDestination(null));
+            } else {
+              console.error("Unable to fetch location details.");
+            }
+          }}
+          query={{
+            key: GOOGLE_MAPS_APIKEY,
+            language: "en"
+          }}
+          minLength={2}
+          nearbyPlacesAPI='GooglePlacesSearch'
+          returnKeyType={'search'}
+          debounce={400}
+          enablePoweredByContainer={false}
         />
-      
-      <NavOptions />
+
+        <NavOptions />
       </View>
     </SafeAreaView>
   );
@@ -53,8 +73,4 @@ const HomeScreen = () => {
 
 export default HomeScreen;
 
-const styles = StyleSheet.create({
-  // text:{
-  //     color:"blue"
-  // }
-});
+const styles = StyleSheet.create({});
--- uberr/screens/Map.js
+++ uberr/screens/Map.js
@@ -2,21 +2,23 @@ import { StyleSheet, Text, View, Dimensions } from 'react-native'
 import React from 'react'
 import MapView,{Marker} from 'react-native-maps'
 import tw from 'tailwind-react-native-classnames'
+import { useSelector } from 'react-redux'
+import { selectOrigin } from '../slices/navSlice'
 
 const Map = () => {
     const { height } = Dimensions.get('window')
     const mapHeight = height / 2
-  
+    const origin = useSelector(selectOrigin)
     return (
       <View>
         <MapView
           style={{ height: mapHeight }}
           mapType='mutedStandard'
           initialRegion={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.015,
-            longitudeDelta: 0.0121,
+            latitude: origin.location.lat,
+            longitude: origin.location.lng,
+            latitudeDelta: 0.005,
+            longitudeDelta: 0.005,
           }}
         >
           {/* Add markers or other map components if needed */}
